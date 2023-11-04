d0b4e349ec4bcda54afd099e9a74c453ca98f173
Joice Cristina <66541373+Joice-crypto@users.noreply.github.com>
2023-11-04 01:27:14 -0300

    Update next.config.js

--- web/nextjs-blog/next.config.js
+++ web/nextjs-blog/next.config.js
@@ -2,5 +2,12 @@
 const nextConfig = {}
 module.exports = {
     distDir: 'build',
+    generateBuildId: async () => {
+    if (process.env.BUILD_ID) {
+      return process.env.BUILD_ID;
+    } else {
+      return `${new Date().getTime()}`;
+    }
+  }
 }
 module.exports = nextConfig

9319dfdb228c093665d3b9229122f8363ba24743
Joice-crypto <joyciinha_cristina@aluno.ufsj.edu.br>
2023-11-27 10:14:49 -0300

    Mudando o host

--- web/nextjs-blog/.next/types/app/page.ts
+++ web/nextjs-blog/.next/types/app/page.ts
@@ -1,4 +1,4 @@
-// File: /home/joice/Área de Trabalho/IC/BemEstar/web/nextjs-blog/src/app/page.tsx
+// File: /home/joice/Área de Trabalho/BemEstar/BemEstar/web/nextjs-blog/src/app/page.tsx
 import * as entry from '../../../src/app/page.js'
 import type { ResolvingMetadata } from 'next/dist/lib/metadata/types/metadata-interface.js'
 

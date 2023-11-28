2c18639be63c800c2189edab90ab88ee781fe9e2
Joice-crypto <joyciinha_cristina@aluno.ufsj.edu.br>
2023-11-27 22:51:53 -0300

    aa

--- web/nextjs-blog/src/lib/api.ts
+++ web/nextjs-blog/src/lib/api.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export const api = axios.create({
-    baseURL:'http://clownfish-app-3x9ir.ondigitalocean.app/:8080'
+    baseURL:'http://clownfish-app-3x9ir.ondigitalocean.app:8080'
 })
